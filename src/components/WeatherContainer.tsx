edd1c736354e971722aced3dfc85ee1fa4a96d6e
Marcoglp98 <marcoglp98@gmail.com>
2023-06-16 20:14:31 +0200

    fix: fix app logic

--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -2,48 +2,43 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const WeatherContainer = () => {
-  const [city, setCity] = useState({
-    name: "",
-    humidity: 0,
-  });
+const [city, setCity] = useState({
+  name: "palermo",
+  humidity:0
+})
 
-  const [input, setInput] = useState("");
+const [input, setInput] = useState("")
 
-  const handleClick = () => {
+const handleClick = ()=> {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4a550628d96845a7da3b7a38a4285d38&units=metric`;
-    axios
-      .get(url)
-      .then((res) => {
-        console.log(res.data);
-        setCity({
-          ...city,
-          name: res.data.name,
-          humidity: res.data.main.humidity,
-        });
-      })
-
-      .catch((error) => alert(`Enter a valid location (${error})`));
-  };
-  return (
+    axios.get(url)
+    .then(res=>{console.log(res.data);
+    setCity({...city, name:res.data.name, humidity: res.data.main.humidity})
+  })
+    
+    .catch( error => alert(`Enter a valid location (${error})`))
+ 
+}
+return (
     <>
       <div>
+      <button onClick={handleClick}>cliccami</button>
         <form>
           <input
-            onChange={(e) => setInput(e.target.value)}
+            onChange={e=> setInput(e.target.value)}
             type="search"
             placeholder="search location"
             name=""
             id=""
+            
           />
-          <button onClick={handleClick}>cliccami</button>
         </form>
-
-       
-        <img  alt=""></img>
+        <h1>{city.name}</h1>
+        <div></div>
+        <img src="https://openweathermap.org/img/wn/10d@2x.png" alt=""></img>
         <h1>{city.humidity}</h1>
       </div>
       <div>
-      <h1>{city.name}</h1>
         <h1>5°</h1>
       </div>
       <div>
